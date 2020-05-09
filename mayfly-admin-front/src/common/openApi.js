f2ae726bb8e090c9ce46d70476b39900ed2be458
meilin.huang <954537473@qq.com>
2020-05-09 15:57:04 +0800

    前端漏提交

--- mayfly-admin-front/src/common/openApi.js
+++ mayfly-admin-front/src/common/openApi.js
@@ -2,6 +2,6 @@ import request from './request.js'
 
 export default {
   login: (param) => request.request('POST', '/open/login', param),
-  captcha:  () => request.request('GET', '/open/captcha'),
+  captcha: () => request.request('GET', '/open/captcha'),
   logout: (param) => request.request('POST', '/sys/accounts/logout/{token}', param),
 }
