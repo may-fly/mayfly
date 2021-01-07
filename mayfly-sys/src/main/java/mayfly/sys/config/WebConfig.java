32940da0a0d89700852aa5ad552b0f262a8b20a8
meilin.huang <954537473@qq.com>
2021-01-07 11:37:45 +0800

    注释无用玩意

--- mayfly-sys/src/main/java/mayfly/sys/config/WebConfig.java
+++ mayfly-sys/src/main/java/mayfly/sys/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SignInterceptor()).addPathPatterns("/**");
+//        registry.addInterceptor(new SignInterceptor()).addPathPatterns("/**");
         registry.addInterceptor(new PermissionInterceptor(permissionService))
                 .addPathPatterns("/sys/**", "/devops/**");
     }
